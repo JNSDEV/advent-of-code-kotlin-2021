ff3521bf539f22a3077918452ae1c31255b46d64
Jonas Ockerman <jonas.ockerman@external.toyota-europe.com>
2021-12-03 15:20:59 +0100

    DayÂ01 - part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,32 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        if (input.isEmpty()) return 0;
+
+        var count = 0;
+        var previous: Int? = null;
+
+        input.forEach {
+            var current: Int = Integer.parseInt(it)
+            previous?.let {
+                if (current > previous!!) {
+                    count += 1
+                }
+            }
+            previous = current;
+        };
+
+
+        return count
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
+// test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println(part1(testInput))
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
